0b1feb8f735b33cf43530b963b87b76404740d4f
Neomaster <the@neomaster.net>
2023-02-27 00:34:00 +0300

    [Client] main style has been improved.

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
