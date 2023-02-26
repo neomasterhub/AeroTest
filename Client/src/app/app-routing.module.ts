7c409e6777ff86d1a79ecaec6eaf919c04a672d3
Neomaster <the@neomaster.net>
2023-02-27 00:29:47 +0300

    [Client] Inventory module

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'inventory',
+  loadChildren: () => import('./inventory/inventory.module').then(m => m.InventoryModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Client/src/app/inventory/inventory.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { InventoryRoutingModule } from './inventory-routing.module';
+import { InventoryComponent } from './inventory.component';
+
+@NgModule({
+  declarations: [
+    InventoryComponent
+  ],
+  imports: [
+    CommonModule,
+    InventoryRoutingModule
+  ]
+})
+export class InventoryModule {
+}
