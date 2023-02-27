e01597f86554b3a5ae080fbb638d08d52f3393d7
Neomaster <the@neomaster.net>
2023-02-27 03:01:04 +0300

    [Client] Profile form website url pattern

--- Client/src/app/profile/profile.component.ts
+++ Client/src/app/profile/profile.component.ts
@@ -25,6 +25,7 @@ export class ProfileComponent {
     ]),
     webSite: new FormControl('', [
       Validators.maxLength(this.maxLength),
+      Validators.pattern('https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)'),
     ]),
   });
 }
